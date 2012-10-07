d1dc2728d3e6b6e47eaba33fd0593a6a62650097
zhang chi <zhangchi@zhangchideMacBook-Pro.local>
2012-10-07 20:27:59 +0800

    Initial Commit

---
+++ hello-oc2/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  hello-oc2
+//
+//  Created by zhang chi on 12-10-7.
+//  Copyright (c) 2012年 zhang chi. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
